a79de60d0af10a9089fe5361b11222290cb23abe
yujo <yujo.dev@gmail.com>
2022-03-01 00:13:38 +0900

    feat: 모듈 분리 및 TodoList 멤버 추가

--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,12 @@
-// To Do List CRUD
+/**
+ * @module todo
+ */
 
-//TODO 할일 목록을 불러올 수 있다.
-//TODO 할일 목록을 추가할 수 있다.
-//TODO 할일 목록을 수정할 수 있다.
-//TODO 할일 목록을 삭제할 수 있다.
+/**
+ * Array of Todo
+ * @type {Todo[]}
+ */
+const todos = []
 
 /**
  * @function getTodos
