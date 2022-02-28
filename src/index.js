aca313d632e31d92523b3029637987593c981707
yujo <yujo.dev@gmail.com>
2022-02-28 23:55:23 +0900

    feat: Todo, Todos 타입 정의

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@
 //TODO 할일 목록을 추가할 수 있다.
 //TODO 할일 목록을 수정할 수 있다.
 //TODO 할일 목록을 삭제할 수 있다.
+
+/**
+ * Todo Object
+ * @type {{id: number|string, title: string, completed: boolean}}
+ */
+const Todo = {}
+
+/**
+ * Todos Array
+ * @type {Todo[]}
+ */
+const Todos = []
+
+/**
+ * @function getTodoList
+ * @description 할일 목록을 불러온다.
+ * @returns Todo[] 할일 목록
+ */
+
+function getTodoList() {}
