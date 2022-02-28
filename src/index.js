55b466d4359dd1e77d07dc65f3d09aa5396da461
yujo <yujo.dev@gmail.com>
2022-02-28 23:25:48 +0900

    chore: 기존 코드 정리 및 구현 사항 정리

--- src/index.js
+++ src/index.js
@@ -1,51 +1,6 @@
-/**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function Book(title, author) {}
+// To Do List CRUD
 
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+//TODO 할일 목록을 불러올 수 있다.
+//TODO 할일 목록을 추가할 수 있다.
+//TODO 할일 목록을 수정할 수 있다.
+//TODO 할일 목록을 삭제할 수 있다.
