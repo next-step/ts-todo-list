e9f107cb7e18761c8204807ddd40ec4883a4f5e2
dahye1013 <alrean@naver.com>
2022-03-01 20:32:23 +0900

    remove: 임시 예외처리 조건

--- src/Todos.js
+++ src/Todos.js
@@ -7,9 +7,8 @@ function Todos() {}
 
 /**
  * @description Todo 인스턴스를 추가합니다. Todo의 id는 유일키로 해당 메소드에서 생성됩니다.
- * @param {Todo} newTodo 신규 TODO 객체
+ * @param {Todo} newTodo 신규 Todo 객체
  * @returns {Array.<Todo>} 신규 등록 결과
- * @throws {InvalidTodoContent} 신규 내용이 빈 값인 경우에 대한 예외처리 - 신규 Todo 내용은 빈 값일 수 없습니다.
  */
 Todos.prototype.addTodo = function () {};
 
@@ -23,7 +22,6 @@ Todos.prototype.findAllTodos = function () {};
  * @description Id에 해당하는 Todo를 찾습니다.
  * @param {string} todoId 찾는 todo id
  * @returns {Todo} 찾은 결과
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
  */
 Todos.prototype.findTodoById = function () {};
 
@@ -31,7 +29,6 @@ Todos.prototype.findTodoById = function () {};
  * @description Id에 해당하는 Todo를 수정 후, 결과 값을 반환합니다.
  * @param {Todo} todo 수정 대상 id
  * @returns {Todo} 수정 결과
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
  */
 Todos.prototype.updateTodoById = function () {};
 
@@ -39,8 +36,6 @@ Todos.prototype.updateTodoById = function () {};
  * @description 해당하는 todId의 Todo에서, tagId에 해당하는 Tag name을 수정합니다.
  * @param {string} todoId 수정하는 Tag의 Todo
  * @param {string} tagId 수정하고자 하는 Todo Id
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
- * @throws {InvalidTagId} 존재하지 않는 tag id에 대한 예외처리
  * @returns {Tag} 수정 된 Tag
  */
 Todos.prototype.updateTagById = function () {};
@@ -49,7 +44,6 @@ Todos.prototype.updateTagById = function () {};
  * @description Todo Id 기준으로 Todo를 삭제합니다.
  * @param {string} todoId 삭제 대상 Todo id
  * @returns {Array.<Todo>} 삭제 결과
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
  */
 Todos.prototype.removeTodoById = function () {};
 
@@ -63,7 +57,6 @@ Todos.prototype.removeAllTodo = function () {};
  * @description Todo id 해당하는 Tag를 전체 삭제합니다.
  * @param {string} todoId 삭제 대상 Todo id
  * @return {Todo} 삭제 결과
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
  */
 Todos.prototype.removeAllTagByTodoId = function () {};
 
@@ -72,6 +65,5 @@ Todos.prototype.removeAllTagByTodoId = function () {};
  * @param {string} todoId 삭제 대상 Todo id
  * @param {string} tagId 삭제 대상 Tag id
  * @returns {Todo} 삭제 결과
- * @throws {InvalidTodoId} 존재하지 않는 todo id에 대한 예외처리
  */
 Todos.prototype.removeTagByTodoIdAndTagId = function () {};
