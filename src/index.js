3683439045a8d396d3e60634c1ac085a4f7de874
bohyunkang <boni@mz.co.kr>
2022-03-01 15:37:36 +0900

    선언부, jsdoc 작성

--- src/index.js
+++ src/index.js
@@ -1,51 +1,47 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * 투두 목록 배열
+ * @type {Todo[]}
  */
-function Book(title, author) {}
+const todos = [];
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * 투두 항목에 들어가야 할 내용
+ * @typedef {Object} Todo
+ * @property {number} id - 아이디(필수)
+ * @property {string} content - 내용(필수)
+ * @property {boolean} isDone - 완료여부(필수)
+ * @property {string} category - 카테고리(필수)
+ * @property {array} tags - 태그들(선택)
  */
-function foo() {}
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
+/** @function getTodos
+ * @description 투두 목록 조회 함수
+ * @todo 모든 할 일을 조회할 수 있다.
+ * @todo ID를 기반으로 특정 할 일을 조회할 수 있다.
  */
-function bar() {}
+function getTodos() {}
 
-/**
- * Generic dairy product.
- * @constructor
+/** @function addTodo
+ * @description 투두 항목 추가 함수
+ * @todo 할 일을 추가할 수 있다.
+ * @todo 내용없이 추가할 수 없다.
  */
-function DairyProduct() {}
+function addTodo() {}
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
+/** @function updateTodo
+ * @description 투두 항목 수정 함수
+ * @param {number} id - 수정할 Todo의 id
+ * @todo ID를 제외한 모든 속성을 수정할 수 있다.
+ * @todo 특정 할 일의 특정 태그를 수정할 수 있다.
  */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+function updateTodo(id) {}
 
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+/** @function deleteTodo
+ * @description 투두 항목 삭제 함수
+ * @param {number} id - 삭제할 Todo의 id
+ * @todo ID를 기반으로 특정 할 일을 삭제할 수 있다.
+ * @todo 모든 할 일을 제거할 수 있다.
+ * @todo 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * @todo 특정 할 일의 모든 태그를 제거할 수 있다.
  */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+function deleteTodo(id) {}
