c88fc2c1ab4f4b89fa93087df1bf04dfe3ff0295
Molly <dngwoodo@gmail.com>
2022-03-01 10:08:11 +0900

    feat: Todo 클래스 추가

--- src/index.js
+++ src/index.js
@@ -1,51 +1,53 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @typedef {Object} TodoType
+ * @property {number} id
+ * @property {string} task
+ * @property {boolean} done
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * @typedef {Object} StateType
+ * @property {number} newId
+ * @property {string} task
+ * @property {TodoType[]} todos
  */
-function foo() {}
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+class Todo {
+  /**
+   * @member {StateType}
+   */
+  state;
 
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+  /**
+   * @param {StateType} initialState
+   */
+  constructor(initialState) {}
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+  /**
+   * @returns {TodoType[]}
+   */
+  get todos() {}
 
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
+  /**
+   * @param {string} newTask input에 입력되는 value
+   */
+  changeTask(newTask) {}
 
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+  addTodo() {}
+
+  /**
+   * @param {number} id
+   */
+  deleteToto(id) {}
+
+  /**
+   * @param {number} id
+   * @param {string} changedTask
+   */
+  changeTodo({ id, changedTask }) {}
+
+  /**
+   * @param {number} id
+   */
+  checkTodo(id) {}
+}
