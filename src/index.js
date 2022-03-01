05ceaf21470dea300ecee9221901514fec63d4b4
Molly <dngwoodo@gmail.com>
2022-03-01 11:21:42 +0900

    feat: 타입에 설명 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /**
+ * 하나의 Todo가 들고 있는 데이터
  * @typedef {Object} TodoType
  * @property {number} id
  * @property {string} task
@@ -6,10 +7,11 @@
  */
 
 /**
+ * Todo 클래스에서 사용되는 상태
  * @typedef {Object} StateType
- * @property {number} newId
- * @property {string} task
- * @property {TodoType[]} todos
+ * @property {number} newId - todo를 생성할 때 필요한 unique id
+ * @property {string} task - todo를 생성할 때 필요한 할일
+ * @property {TodoType[]} todos - 생성된 todo를 관리하는 배열
  */
 
 class Todo {
@@ -19,7 +21,7 @@ class Todo {
   state;
 
   /**
-   * @param {StateType} initialState
+   * @param {StateType} initialState - Todo 상태의 초기값
    */
   constructor(initialState) {}
 
@@ -29,25 +31,25 @@ class Todo {
   get todos() {}
 
   /**
-   * @param {string} newTask input에 입력되는 value
+   * @param {string} newTask - 뱐경할 task 값
    */
   changeTask(newTask) {}
 
   addTodo() {}
 
   /**
-   * @param {number} id
+   * @param {number} id - 삭제할 todo id 값
    */
   deleteToto(id) {}
 
   /**
-   * @param {number} id
-   * @param {string} changedTask
+   * @param {number} id - 변경할 todo id 값
+   * @param {string} changedTask - 변경할 todo task 값
    */
   changeTodo({ id, changedTask }) {}
 
   /**
-   * @param {number} id
+   * @param {number} id - 변경할 todo id 값
    */
   checkTodo(id) {}
 }
