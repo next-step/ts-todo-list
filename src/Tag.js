33c3764263138f400d40cf98e719d29e1ddb9b35
Shin Dahye <64780560+dahye1013@users.noreply.github.com>
2022-03-02 18:24:44 +0900

    [TP-TS] To Do 미션 Step1 - 신다혜 (#7)
    * remove: 불필요한 파일 삭제
    
    * feat: 기본 데이터 인스턴스 및 내장 함수 모델링 (Todos, Todo, Tag)
    
    * remove: 임시 예외처리 조건
    
    * refactor: id 타입 string에서 number로 일괄 수정
    
    * feat: custom-template dashdoc 설정
    
    * chore: build 업로드
    
    * refactor: remove시 반환값 일관성있게 수정

---
+++ src/Tag.js
@@ -0,0 +1,15 @@
+/**
+ * @description Tag 생성자입니다.
+ * @constructor
+ * @param {Tag} Tag
+ * @param {number} Tag.id Tag 아이디
+ * @param {string} Tag.name Tag 명
+ */
+function Tag() {}
+
+/**
+ * @description Tag 명을 수정합니다. id는 수정 할 수 없습니다.
+ * @param {string} name Tag 명
+ * @return {void}
+ */
+Tag.prototype.updateTag = function () {};
