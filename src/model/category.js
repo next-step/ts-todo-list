28e573fe4c44cda6b370ea3cd64f1bd2d2abb701
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2022-03-03 01:20:07 +0900

    feat: todo 프로젝트 설계 및 JSDoc 문서화

---
+++ src/model/category.js
@@ -0,0 +1,9 @@
+class Category {
+  /**
+   * @constructor
+   * @param {number} initCategory
+   * @param {number} initCategory.id - 카테고리 아이디.
+   * @param {string} initCategory.name - 카테고리 이름. 중복될 수 없다.
+   */
+  constructor(initCategory) {}
+}
---
+++ src/model/tag.js
@@ -0,0 +1,9 @@
+class Tag {
+  /**
+   * @constructor
+   * @param {number} initTag
+   * @param {number} initTag.id - 태그의 아이디
+   * @param {string} initTag.name - 태그의 이름. 중복될 수 없다.
+   */
+  constructor(initTag) {}
+}
