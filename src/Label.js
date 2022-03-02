52884ad6ca1c0301f444efbba2e7c8fe30db09e2
g1moon <gmoon.cho@gmail.com>
2022-03-02 19:26:51 +0900

    feat: (구조만) Label Class 추가(todo의 labels의 아이템 값)
      - feat: label의 내용을 get하는 기능 추가(get data)
    
      - feat: label의 내용을 set하는 기능 추가(set content)

---
+++ src/Label.js
@@ -0,0 +1,37 @@
+/**
+ * @file 라벨을 생성하는 Label 클래스
+ * @author guymoon(조기문)
+ */
+
+/**
+ * Create a new label
+ * @class
+ * @classdesc 라벨을 생성하는 Label 클래스
+ */
+class Label {
+  /**
+   * @type {{id: number, content: string}}
+   */
+  #data = {};
+
+  /**
+   * @constructor
+   * @param {number} id
+   * @param {string} content
+   */
+  constructor(id, content) {}
+
+  /**
+   * @return {{id: number, content: string}}
+   */
+  get data() {
+    return this.#data;
+  }
+
+  /**
+   * @param {string} value
+   */
+  set content(value) {
+    this.#data.content = value;
+  }
+}
