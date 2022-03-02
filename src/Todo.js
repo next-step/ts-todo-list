76a03a61e1d50c7b36d118ba69dba606c8fb5114
g1moon <gmoon.cho@gmail.com>
2022-03-02 19:26:37 +0900

    feat: (구조만) Todo Class 추가
      - feat: 할 일의 내용을 get하는 기능 추가(get data)
    
      - feat: 할 일 내용 set하는 기능 추가(set content)
      - feat: 할 일 완료여부 set하는 기능 추가(set isDone)
      - feat: 할 일 중요도 set하는 기능 추가(set priority)
      - feat: 할 일 라벨들 set하는 기능 추가(set labels)

---
+++ src/Todo.js
@@ -0,0 +1,71 @@
+/**
+ * @file 할 일을 만드는 Todo 클래스
+ * @author guymoon(조기문)
+ */
+
+/**
+ * Create a new todo
+ * @class
+ * @classdesc 할 일을 만드는 Todo 클래스
+ */
+class Todo {
+  /**
+   * @type {{id: number, isDone: boolean, content: string, labels: Label[]}}
+   */
+  #data = {};
+
+  /**
+   * @constructor
+   * @param {number} id - 할 일의 id
+   * @param {string} content - 할 일의 내용
+   */
+  constructor(id, content) {
+    this.#data.id = id;
+    this.#data.content = content;
+  }
+
+  /**
+   * @function createTodoUniqueId
+   * @description (getter) 할 일의 unique한 id를 만듭니다
+   * @return {object} TodoDescription
+   */
+  get data() {
+    return this.#data;
+  }
+
+  /**
+   * @function content
+   * @description (setter) 할 일의 content(내용) 값을 갱신합니다.
+   * @param {string} value
+   */
+  set content(value) {
+    this.#data.content = value;
+  }
+
+  /**
+   * @function isDone
+   * @description (setter) 할 일의 isDone(완료 여부) 값을 갱신합니다.
+   * @param {boolean} value
+   */
+  set isDone(value) {
+    this.#data.isDone = value;
+  }
+
+  /**
+   * @function priority
+   * @description (setter) 할 일의 ㅎ(중요도) 값을 갱신합니다.
+   * @param {('LOW' | 'MIDDLE' | 'HIGH')} value
+   */
+  set priority(value) {
+    this.#data.priority = value;
+  }
+
+  /**
+   * @function labels
+   * @description (setter) 할 일의 labels 값을 갱신합니다.
+   * @param {Label[]} value
+   */
+  set labels(value) {
+    this.#data.labels = value;
+  }
+}
