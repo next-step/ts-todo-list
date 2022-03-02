e1f169a740f050ecd9717b3a212579966be7bc1b
0imbean0 <75007375+0imbean0@users.noreply.github.com>
2022-03-02 22:12:19 +0900

    To Do 미션 step1 설계 (#13)
    Co-authored-by: seonbin.im <seonbin.im@yanolja.com>

--- src/index.js
+++ src/index.js
@@ -1,51 +1,151 @@
-/**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function Book(title, author) {}
+/** 할 일 목록 */
+class TodoList {
+    /**
+     * 할 일 목록
+     * @member {Array.<TodoItem>}
+     */
+    list = []
 
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
+    /**
+     * 할 일을 추가할 수 있다.
+     * 내용없이 추가할 수 없다.
+     * @param {TodoItem} todoItem - 할 일 목록에 추가할 할 일 객체
+     */
+    addItem(todoItem) {
+        // ...
+    }
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+    /**
+     * ID를 기반으로 특정 할 일을 조회할 수 있다.
+     * @param {string} id - 조회할 일의 ID
+     * @return {TodoItem} - ID를 기반으로 찾은 할 일
+     */
+    readItemById(id) {
+        // ...
+    }
 
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+    /**
+     * 모든 할 일을 조회할 수 있다.
+     * @return {Array.<TodoItem>} - 할 일 목록
+     */
+    readItemAll() {
+        // ...
+    }
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+    /**
+     * ID를 기반으로 특정 할 일을 수정할 수 있다.
+     * ID를 제외한 모든 속성을 수정할 수 있다.
+     * @param {string} id - 수정할 일의 ID
+     * @param {Object} option - 수정할 일의 속성과 내용
+     */
+    updateItemById(id, option) {
+        // ...
+    }
 
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
+    /**
+     * ID를 기반으로 특정 할 일을 삭제할 수 있다.
+     * @param {string} id - 삭제할 일의 ID
+     */
+    deleteItemById(id) {
+        // ...
+    }
 
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+    /**
+     * 모든 할 일을 제거할 수 있다.
+     */
+    deleteItemAll() {
+        // ...
+    }
+
+}
+
+/** 할 일 */
+class TodoItem {
+    /**
+     * ID - 랜덤한 아이디
+     * @member {string}
+     */
+    id
+
+    /**
+     * 내용
+     * @member {string}
+     */
+    content
+
+    /**
+     * 카테고리
+     * @member {string}
+     */
+    category
+
+    /**
+     * 완료여부
+     * @member {boolean}
+     * @default
+     */
+    isFinished = false
+
+    /**
+     * 태그 목록
+     * @member {Array.<Tag>}
+     */
+    tags = []
+
+    /**
+     * TodoItem을 생성한다.
+     * @param {!string} content - 할 일 내용
+     * @param {!string} category - 할 일 카테고리
+     * @param {?Array.<string>} tagContents - 할 일 태그 목록의 태그 내용들
+     */
+    constructor(content, category, tagContents) {
+        // ...
+    }
+
+    /**
+     * ID를 기반으로 특정 태그를 수정할 수 있다.
+     * @param {string} id - 수정할 태그의 ID
+     * @param {string} content - 수정할 태그의 내용
+     */
+    updateTagById(id, content) {
+        // ...
+    }
+
+    /**
+     * ID를 기반으로 특정 태그를 삭제할 수 있다.
+     * @param {string} id - 삭제할 일의 ID
+     */
+    deleteTagById(id) {
+        // ...
+    }
+
+    /**
+     * 모든 태그를 제거할 수 있다.
+     */
+    deleteTagAll() {
+        // ...
+    }
+}
+
+/** 태그 */
+class Tag {
+    /**
+     * ID - 랜덤한 아이디
+     * @member {string}
+     */
+    id
+
+    /**
+     * 내용
+     * @member {string}
+     */
+    content
+
+    /**
+     * Tag를 생성한다.
+     * @param {string} content - 할 일 내용
+     */
+    constructor(content) {
+        // ...
+    }
+}
