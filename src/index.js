b4ed43dab9de3c2e6eef1e1054b5daf31b3b66ea
ritajeong <ritajeong.dev@gmail.com>
2022-03-02 23:51:37 +0900

    To Do 미션 Step1

--- src/index.js
+++ src/index.js
@@ -1,51 +1,109 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * Todoitem를 가지는 배열 객체
+ * @type {Todoitem[]}
  */
-function Book(title, author) {}
+const Todoitems = [];
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * Todoitem 객체
+ * 순서대로 아이디, 내용, 완료여부, 카테고리, 태그들(문자열 타입의 태그를 담은 문자열 배열)
+ * @type {{id: !string, content: !string, completed: !boolean, category: !string, tags: ?string[]}}
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+const Todoitem = {};
 
 /**
- * Generic dairy product.
- * @constructor
+ * 할 일을 추가할 수 있다. 내용없이 추가할 수 없다.
+ * Todoitem 객체를 생성한다.
+ * @param id {string} - required
+ * @param content {string} - required
+ * @param completed {boolean} - required
+ * @param category {string} - required
+ * @param tags {?string[]} - optional
+ * @returns {{}}
  */
-function DairyProduct() {}
+const createItem = (id, content, completed, category, tags) => {
+	return {};
+}
 
 /**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
+ * 모든 할 일을 조회할 수 있다.
+ * Todoitems를 반환한다.
+ * @returns {Todoitem[]}
  */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+const readItems = () => {
+	return [];
+}
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * ID를 기반으로 특정 할 일을 조회할 수 있다.
+ * 해당 ID를 가진 Todoitem 인스턴스를 반환한다.
+ * @param id
+ * @returns {{}}
  */
-function Milk() {}
+const readItem = (id) => {
+	return {};
+}
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * ID를 제외한 모든 속성을 수정할 수 있다.
+ * 수정 성공시 true 반환
+ * undefined 입력시 해당 속성은 수정하지 않음
+ * @param id {string} - required
+ * @param content {string} - required
+ * @param completed {boolean} - required
+ * @param category {string} - required
+ * @param tags {string[]} - optional
+ * @returns {boolean}
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+const updateItem = (id, content, completed, category, tags) => {
+	return true;
+}
+
+/**
+ * 특정 할 일의 특정 태그를 수정할 수 있다.
+ * 수정 성공시 true 반환
+ * @param id
+ * @param property - 수정하려는 속성
+ * @param tobe - 수정할 내용
+ * @returns {boolean}
+ */
+const updateTag = (id, property, tobe) => {
+	return true;
+}
+
+/**
+ * ID를 기반으로 특정 할 일을 삭제할 수 있다.
+ * 삭제 성공시 true 반환
+ * @param id
+ * @returns {boolean}
+ */
+const deleteItem = (id) => {
+	return true;
+}
+
+/**
+ * 모든 할 일을 제거할 수 있다.
+ * @returns {boolean}
+ */
+const deleteItems = () => {
+	return true;
+}
+
+/**
+ * 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * @param id
+ * @param tag
+ * @returns {boolean}
+ */
+const deleteTag = (id, tag) => {
+	return true;
+}
+
+/**
+ * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * @param id
+ * @returns {boolean}
+ */
+const deleteTags = (id) => {
+	return true;
+}
