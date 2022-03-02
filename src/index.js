a5df15b32f122f1984aa07c4fe66e77be1e51d93
SunYoungKwon <sunnyk5780@gmail.com>
2022-03-02 23:24:46 +0900

    feat: 요구사항 설계

--- src/index.js
+++ src/index.js
@@ -1,51 +1,120 @@
-/**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function Book(title, author) {}
+class Todo {
+  /**
+   * Create Todo
+   * @constructor
+   * @param {number} id - 할 일의 내용
+   * @param {string} text - 할 일의 내용
+   * @param {string} category - 할 일의 카테고리
+   * @param {string[]} [tagList=[]] - 할 일의 태그들
+   */
+  constructor(id, text, category, tagList) {}
 
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
+  /**
+   * 할 일의 내용을 변경한다.
+   * @param {string} text - 변경할 할 일 내용
+   */
+  updateText(text) {}
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+  /**
+   * 할 일의 카테고리를 변경한다.
+   * @param {string} category - 새로운 카테고리
+   */
+  updateCategory(category) {}
 
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+  /**
+   * 할 일의 태그 목록을 변경한다.
+   * @param {string[]} tagList - 새로운 태그 목록
+   */
+  updateTagList(tagList) {}
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+  /**
+   * 할 일의 완료 상태를 토글한다.
+   */
+  toggleCompleted() {}
+}
 
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
+class TodoList {
+  /**
+   * Create TodoList
+   * @constructor
+   * @param {Todo[]} [initTodoList=[]] - 초기값으로 설정할 할 일 목록
+   */
+  constructor(initTodoList) {}
 
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+  /**
+   * 모든 할 일 목록을 조회한다.
+   * @returns {Todo[]}
+   */
+  getTodoList() {}
+
+  /**
+   * 특정 할 일을 조회한다.
+   * @param {number} id
+   * @returns {Todo}
+   */
+  getTodoById(id) {}
+
+  /**
+   * 새로운 할 일을 추가한다.
+   * @param {Todo} todo - 새로 추가할 할 일
+   */
+  addTodo(todo) {}
+
+  /**
+   * 특정 할 일에 태그를 추가한다.
+   * @param {number} id
+   * @param {string} tag
+   */
+  tagTodo(id, tag) {}
+
+  /**
+   * 특정 할 일 내용을 변경한다.
+   * @param {number} id
+   * @param {string} text
+   */
+  updateTodoText(id, text) {}
+
+  /**
+   * 특정 할 일의 완료 상태를 토글한다.
+   * @param {number} id - 완료 상태를 변경할 할 일의 id
+   */
+  toggleTodoCompleted(id) {}
+
+  /**
+   * 특정 할 일 카테고리를 변경한다.
+   * @param {number} id - 카테고리를 변경할 할 일의 id
+   * @param {string} category - 새로운 카테고리
+   */
+  updateTodoCategory(id, category) {}
+
+  /**
+   * 특정 할 일 태그 목록을 변경한다.
+   * @param {number} id - 태그를 변경할 할 일의 id
+   * @param {string[]} tagList - 새로운 태그 목록
+   */
+  updateTodoTagList(id, tagList) {}
+
+  /**
+   * 특정 할 일을 삭제한다.
+   * @param {number} id - 삭제할 할 일의 id
+   */
+  removeTodo(id) {}
+
+  /**
+   * 모든 할 일을 삭제한다.
+   */
+  removeAllTodo() {}
+
+  /**
+   * 특정 할 일의 특정 태그를 삭제한다.
+   * @param {number} id - 태그를 삭제할 할 일의 id
+   * @param {string} tag - 삭제할 태그의 텍스트
+   */
+  removeTodoTag(id, tag) {}
+
+  /**
+   * 특정 할 일의 모든 태그를 삭제한다.
+   * @param {number} id - 모든 태그를 삭제할 할 일의 id
+   */
+  removeTodoAllTag(id) {}
+}
