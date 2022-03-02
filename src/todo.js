4b36873b09a1414d570fda71acdb37538c1e7c57
ppamppamman <ryou9305@naver.com>
2022-03-02 23:07:20 +0900

    fix: docs에 필요한 todo 정의

---
+++ src/todo.js
@@ -0,0 +1,53 @@
+/**
+ * @file todo.js Typing TypeScript 1기, To Do - Begin TS 정의
+ * @author ppamppamman(유재욱)
+ */
+
+/**
+ * @module Todo Module
+ */
+
+/**
+ * Array of Todo
+ * @type {Todo[]}
+ */
+const todos = [];
+
+/**
+ * @function getTodos
+ * @description 모든 할일을 반환한다.
+ * @returns - {Todo[]} 모든 할일
+ */
+
+function getTodos() {}
+
+/**
+ * @function addTodo
+ * @param {Object} todo - 할일 Object
+ * @param {string} todo.id - 할일의 id
+ * @param {string} todo.title -  할일의 title
+ * @param {boolean} todo.isComplete - 할일의 완료 여부
+ * @description 할 일을 추가한다.
+ */
+
+function addTodo(todo) {}
+
+/**
+ * @function deleteTodo
+ * @param {string} id - 삭제할 할일의 id
+ * @description 같은 id의 할일 삭제
+ * @returns {boolean} 삭제 결과
+ */
+
+function deleteTodo(id) {}
+
+/**
+ * @function editTodo
+ * @param {Object} todo - 할일 Object
+ * @param {string} todo.id - 할일의 id
+ * @param {string} todo.title - 할일의 title
+ * @param {boolean} todo.isComplete - 할일의 완료 여부
+ * @description 할일 목록에서 todo를 받아 edit처리를 한다. 완료여부isComplete와 제목title을 수정할 수 있다.
+ */
+
+function editTodo(todo) {}
