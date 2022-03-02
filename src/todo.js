77a625f12d2d0a094d9b1e6241a1bdd71bc80ed9
ppamppamman <ryou9305@naver.com>
2022-03-02 23:12:27 +0900

    fix: getTodo와 일부 메서드의 return 정의

--- src/todo.js
+++ src/todo.js
@@ -22,12 +22,22 @@ const todos = [];
 function getTodos() {}
 
 /**
+ * @function getTodo
+ * @param {string} id - 불러올 할일의 id
+ * @description id로 접근하여 특정 할 일을 반환한다.
+ * @returns - {Todo | null} id값과 매칭되는 할일 혹은 null
+ */
+
+function getTodo(id) {}
+
+/**
  * @function addTodo
  * @param {Object} todo - 할일 Object
  * @param {string} todo.id - 할일의 id
  * @param {string} todo.title -  할일의 title
  * @param {boolean} todo.isComplete - 할일의 완료 여부
  * @description 할 일을 추가한다.
+ * @returns {boolean} 추가 결과
  */
 
 function addTodo(todo) {}
@@ -48,6 +58,7 @@ function deleteTodo(id) {}
  * @param {string} todo.title - 할일의 title
  * @param {boolean} todo.isComplete - 할일의 완료 여부
  * @description 할일 목록에서 todo를 받아 edit처리를 한다. 완료여부isComplete와 제목title을 수정할 수 있다.
+ * @returns {boolean} 수정 결과
  */
 
 function editTodo(todo) {}
