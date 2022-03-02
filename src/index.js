7845e6219286e01e133452b05d39b0d0acf2f435
ppamppamman <ryou9305@naver.com>
2022-03-02 22:52:09 +0900

    fix: index.js init

--- src/index.js
+++ src/index.js
@@ -1,51 +1,25 @@
-/**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function Book(title, author) {}
+/*
+TODO {
+  아이디(required),
+  내용(required),
+  완료여부(required),
+  카테고리(required),
+  태그들(optional),
+}
 
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
+CREATE
+할 일을 추가할 수 있다.
+내용없이 추가할 수 없다.
+READ
+모든 할 일을 조회할 수 있다.
+ID를 기반으로 특정 할 일을 조회할 수 있다.
+UPDATE
+ID를 제외한 모든 속성을 수정할 수 있다.
+특정 할 일의 특정 태그를 수정할 수 있다.
+DELETE
+ID를 기반으로 특정 할 일을 삭제할 수 있다.
+모든 할 일을 제거할 수 있다.
+특정 할 일의 특정 태그를 삭제할 수 있다.
+특정 할 일의 모든 태그를 제거할 수 있다.
 
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+*/
