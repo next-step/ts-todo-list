0217fe168c39ba00a4f6a152a64881528468e16a
tiaz0128 <tiaz0128.dev@gmail.com>
2022-03-04 14:06:54 +0900

    To Do 미션 Step2

--- src/Todo.js
+++ src/Todo.js
@@ -46,19 +46,28 @@ class Todo {
    */
   nextCategoryId
 
-  constructor() { }
+  constructor() {
+    this.todoList = [];
+    this.nextTaskId = 0;
+    this.nextCategoryId = 0;
+  }
 
   /**
    * @description getUniqueTaskId함수를 호출하기전 nextTaskId를 증가시킵니다.
    * @returns {void}
    */
-  setIncreasingTaskId() { }
+  setIncreasingTaskId() {
+    this.nextTaskId++;
+  }
 
   /**
    * @description setIncreasingTaskId함수를 호출 후 nextTaskId를 리턴합니다.
    * @returns {number}
    */
-  getUniqueTaskId() { }
+  getUniqueTaskId() {
+    this.setIncreasingTaskId();
+    return this.nextTaskId;
+  }
 
   /**
    * @description TodoList배열 안에 있는 객체중 하나인 TodoItem객체 안에있는 배열인 tasks배열안에 Task객체를 생성합니다.
@@ -66,7 +75,14 @@ class Todo {
    * @param {string} task
    * @returns {void}
    */
-  createTask() { }
+  createTask(categoryId, task) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    this.todoList[targetTodoItemIndex].tasks.push({
+      taskId: this.getUniqueTaskId(), 
+      task, 
+      isCompleted: false
+    })
+  }
 
   /**
    * @description 하나의 Task객체에 task(할일)를 수정합니다.
@@ -75,7 +91,13 @@ class Todo {
    * @param {string} task
    * @returns {void}
    */
-  updateTask() { }
+  updateTask(categoryId, taskId, task) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    const tasks = this.todoList[targetTodoItemIndex].tasks;
+    const targetTaskIndex = tasks.findIndex((task) => task.taskId === taskId);
+
+    tasks[targetTaskIndex].task = task;
+  }
 
   /**
    * @description 하나의 Task객체를 제거합니다.
@@ -83,7 +105,13 @@ class Todo {
    * @param {number} taskId
    * @returns {void}
    */
-  deleteTask() { }
+  deleteTask(categoryId, taskId) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    const tasks = this.todoList[targetTodoItemIndex].tasks;
+    const targetTaskIndex = tasks.findIndex((task) => task.taskId === taskId);
+
+    tasks.splice(targetTaskIndex, targetTaskIndex + 1)
+  }
 
   /**
    * @description Task객체의 isCompleted 속성이 true이면 false, false면 true로 수정합니다.
@@ -91,26 +119,43 @@ class Todo {
    * @param {number} taskId
    * @returns {void}
    */
-  toggleCompleteTask() { }
+  toggleCompleteTask(categoryId, taskId) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    const tasks = this.todoList[targetTodoItemIndex].tasks;
+    const targetTaskIndex = tasks.findIndex((task) => task.taskId === taskId);
+
+    tasks[targetTaskIndex].isCompleted = !tasks[targetTaskIndex].isCompleted 
+  }
 
   /**
    * getUniqueCategoryId 호출하기전 nextCategoryId를 증가시킵니다.
    * @returns {void}
    */
-  setIncreasingCategoryId() { }
+  setIncreasingCategoryId() {
+    this.nextCategoryId++;
+  }
 
   /**
    * @description setIncreasingCategoryId 함수를 호출 후 nextCategoryId를 리턴합니다.
    * @returns {number}
    */
-  getUniqueCategoryId() { }
+  getUniqueCategoryId() {
+    this.setIncreasingCategoryId();
+    return this.nextCategoryId;
+  }
 
   /**
    * @description TodoList안에 새로운 카테고리 객체를 만듭니다.
    * @param {string} categoryName
    * @returns {void}
    */
-  createCategory() { }
+  createCategory(categoryName) {
+    this.todoList.push({
+      categoryId : this.getUniqueCategoryId(),
+      categoryName,
+      tasks: []
+      })
+  }
 
   /**
    * @description 같은 categoryId의 카테고리명을 수정합니다.
@@ -118,19 +163,30 @@ class Todo {
    * @param {string} categoryName
    * @returns {void}
    */
-  updateCategoryName() { }
+  updateCategoryName(categoryId, categoryName) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    this.todoList[targetTodoItemIndex].categoryName = categoryName;
+  }
 
   /**
    * @description 같은 categoryId의 카테고리명을 제거합니다.
    * @param {number} categoryId
    * @returns {void}
    */
-  deleteCategory() { }
+  deleteCategory(categoryId) {
+    const targetTodoItemIndex = this.todoList.findIndex((todoItem) => todoItem.categoryId === categoryId);
+    this.todoList.splice(targetTodoItemIndex, targetTodoItemIndex + 1)
+  }
 
   /**
    * @description todoList를 콘솔에 출력합니다. (optional로 메시지 출력가능)
    * @param {string} [message]
    * @returns {void}
    */
-  readAllTodo() { }
+  readAllTodo(message) {
+    if(message) console.log('\n' + message)
+    console.log(JSON.stringify(this.todoList))
+  }
 }
+
+exports.Todo = Todo;
