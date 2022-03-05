66a0ee0b309b1dd09b78f75888cc34654571f8a5
빰빰맨 <ppamppamman@gmail.com>
2022-03-05 18:30:45 +0900

    [TP-TS] To Do 미션 Step1 - 유재욱 (#16)
    * fix: index.js init
    
    * fix: docs initialize 및 메서드 정의
    
    * fix: docs에 필요한 todo 정의
    
    * fix: getTodo와 일부 메서드의 return 정의
    
    * fix: title -> contents 프로퍼티 변경
    
    * fix: 할일 전체 삭제 추가
    
    * fix: docs 구조 변경
    
    * fix: tag 변경
    
    * fix: addTodo 프로퍼티 변경
    
    * fix: 함수명 오타 변경
    
    * fix: docs 변경

--- src/index.js
+++ src/index.js
@@ -1,51 +1,86 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @file todo.js Typing TypeScript 1기, To Do - Begin TS 정의
+ * @author ppamppamman(유재욱)
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * 할일 목록
+ * @type {Todo[]}
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
+const todos = [];
 
 /**
- * Generic dairy product.
- * @constructor
+ * 태그 목록
+ * @type {Tag[]}
  */
-function DairyProduct() {}
+const tags = [];
 
 /**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
+ * @function getTodos
+ * @description 모든 할일을 반환한다.
+ * @returns - {Todo[]} 모든 할일
  */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+
+function getTodos() {}
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * @function getTodo
+ * @param {number} id - 불러올 할일의 id
+ * @description id로 접근하여 특정 할 일을 반환한다.
+ * @returns - {Todo | null} id값과 매칭되는 할일 혹은 null
  */
-function Milk() {}
+
+function getTodo(id) {}
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * @function addTodo
+ * @param {Object} todo - 할일 Object
+ * @param {number} todo.id - 할일의 id, 이때 id는 무한히 1씩 증가한다.
+ * @param {string} todo.contents -  할일의 contents
+ * @param {boolean} todo.isComplete - 할일의 완료 여부
+ * @param {Tag[]} todo.tags - 지정된 태그
+ * @description 할 일을 추가한다.
+ * @returns {boolean} 추가 결과
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+
+function addTodo(todo) {}
+
+/**
+ * @function deleteTodo
+ * @param {string} id - 삭제할 할일의 id
+ * @description 같은 id의 할일 삭제
+ * @returns {boolean} 삭제 결과
+ */
+
+function deleteTodo(id) {}
+
+/**
+ * @function deleteTodos
+ * @description 모든 할일 삭제
+ * @returns {boolean} 삭제 결과
+ */
+
+function deleteTodos() {}
+
+/**
+ * @function updateTodo
+ * @param {Object} todo - 할일 Object
+ * @param {number} todo.id - 할일의 id
+ * @param {string} todo.contents - 할일의 contents
+ * @param {boolean} todo.isComplete - 할일의 완료 여부
+ * @description 할일 목록에서 todo를 받아 update처리를 한다. 완료여부isComplete와 내용contents을 수정할 수 있다.
+ * @returns {boolean} 수정 결과
+ */
+
+function updateTodo(todo) {}
+
+/**
+ * @function updateTag
+ * @param {Object} tag - 태그 Object
+ * @param {number} todo.id - 태그의 id
+ * @param {string} todo.name - 태그의 이름
+ * @description 태그 목록에서 태그를 받아 update처리를 한다.
+ * @returns {boolean} 수정 결과
+ */
+
+function updateTag(tag) {}
