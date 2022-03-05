243225d78e215f50934c7172fbda2544a6c6a69b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2022-03-05 23:47:08 +0900

    feat: Tag 클래스 구현

--- src/Tag.js
+++ src/Tag.js
@@ -1,25 +1,27 @@
-class Tag {
-    /**
-     * @constructor
-     * @param {number} id - 태그 아이디.
-     * @param {string} content - 태그 내용.
-     */
-    constructor(id,content) {
-    }
+export default class Tag {
+  /**
+   * @constructor
+   * @param {number} id - 태그 아이디.
+   * @param {string} content - 태그 내용.
+   */
+  constructor(id, content) {
+    this.id = id;
+    this.content = content;
+  }
 
-    /**
-     * @return {string} 태그 정보를 반환합니다
-     */
-    get info(){
+  /**
+   * @return {string} 태그 정보를 반환합니다
+   */
+  get info() {
+    //memo: 태그정보를 반환하라는 게 id, content 모두를 의미하는 걸까요?
+    return this;
+  }
 
-    }
-
-    /**
-     * 태그 내용을 수정합니다.
-     * @param {string} tagContent - 태그 내용.
-     */
-    updateContent(tagContent){
-
-    }
-
+  /**
+   * 태그 내용을 수정합니다.
+   * @param {string} tagContent - 태그 내용.
+   */
+  updateContent(tagContent) {
+    this.content = tagContent;
+  }
 }
