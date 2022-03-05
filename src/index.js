2e6478ed01f7d0b768e8b421d701c987e5211483
SunYoungKwon <sunnyk5780@gmail.com>
2022-03-05 21:53:44 +0900

    feat: step2 구현

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
  * Todo 클래스에서 사용되는 상태
  * @typedef {Object} StateType
  * @property {number} newId - todo를 생성할 때 필요한 unique id
- * @property {string} task - todo를 생성할 때 필요한 할일
+ * @property {string} task - todo를 생성할 때 필요한 할 일
  * @property {TodoType[]} todos - 생성된 todo를 관리하는 배열
  */
 
@@ -23,33 +23,66 @@ class Todo {
   /**
    * @param {StateType} initialState - Todo 상태의 초기값
    */
-  constructor(initialState) {}
+  constructor(initialState) {
+    this.newId = initialState.newId;
+    this.task = initialState.task;
+    this.todos = initialState.todos;
+  }
 
   /**
    * @returns {TodoType[]}
    */
-  get todos() {}
+  get todos() {
+    return this.todos;
+  }
 
   /**
-   * @param {string} newTask - 뱐경할 task 값
+   * @param {string} newTask - 변경할 task 값
    */
-  changeTask(newTask) {}
+  changeTask(newTask) {
+    this.task = newTask;
+  }
 
-  addTodo() {}
+  addTodo() {
+    this.todos = [
+      ...this.todos,
+      { id: this.newId, task: this.task, done: false },
+    ];
+
+    this.newId += 1;
+  }
 
   /**
    * @param {number} id - 삭제할 todo id 값
    */
-  deleteToto(id) {}
+  deleteToto(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 
   /**
    * @param {number} id - 변경할 todo id 값
    * @param {string} changedTask - 변경할 todo task 값
    */
-  changeTodo({ id, changedTask }) {}
+  changeTodo({ id, changedTask }) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+
+      return { ...todo, task: changedTask };
+    });
+  }
 
   /**
    * @param {number} id - 변경할 todo id 값
    */
-  checkTodo(id) {}
+  checkTodo(id) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+
+      return { ...todo, done: !todo.done };
+    });
+  }
 }
