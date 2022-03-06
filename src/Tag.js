d3ccc9685053359c3ac753e3e4e09a0c5783c22c
JO YUN HO <yujo.dev@gmail.com>
2022-03-06 20:25:48 +0900

    [TP-TS] To Do 미션 Step2 - 조윤호 (#23)
    * feat: Todo 구현
    
    * feat: Tag 구현
    
    * feat: Todos 구현
    
    * feat: export functions
    
    * refactor: Todos 구현부 일부 수정
    
    * feat: 동작 테스트 코드 작성
    
    * feat: logger 추가
    
    * chore: 이모지 추가

--- src/Tag.js
+++ src/Tag.js
@@ -5,11 +5,16 @@
  * @param {number} Tag.id Tag 아이디
  * @param {string} Tag.name Tag 명
  */
-function Tag() {}
+export default function Tag({ id, name }) {
+  this.id = id;
+  this.name = name;
+}
 
 /**
  * @description Tag 명을 수정합니다. id는 수정 할 수 없습니다.
  * @param {string} name Tag 명
  * @return {void}
  */
-Tag.prototype.updateTag = function () {};
+Tag.prototype.updateTag = function (name) {
+  this.name = name;
+};
--- src/index.js
+++ src/index.js
@@ -3,3 +3,57 @@
  * @author Dahye Shin
  * @see {@link https://github.com/dahye1013/ts-todo-list}
  */
+
+import Tag from "./Tag.js";
+import Todo from "./Todo.js";
+import Todos from "./Todos.js";
+
+const todos = new Todos();
+
+console.log("🚀🚀🚀 TODO 동작 테스트 🚀🚀🚀");
+
+todos.addTodo(
+  new Todo({
+    id: 1,
+    content: "첫번째 Todo",
+    complete: false,
+    category: "카테고리1",
+    tags: [
+      new Tag({ id: 1, name: "태그1" }),
+      new Tag({ id: 2, name: "태그2" }),
+      new Tag({ id: 3, name: "태그3" }),
+    ],
+  })
+);
+
+todos.addTodo(
+  new Todo({
+    id: 2,
+    content: "두번째 Todo",
+    complete: false,
+    category: "카테고리1",
+    tags: [],
+  })
+);
+
+todos.addTodo(
+  new Todo({
+    id: 3,
+    content: "세번째 Todo",
+    complete: false,
+    category: "카테고리1",
+    tags: [],
+  })
+);
+
+todos.findAllTodos();
+
+todos.findTodoById(1);
+
+todos.removeTagByTodoIdAndTagId(1, 1);
+
+todos.removeAllTagByTodoId(1);
+
+todos.removeTodoById(1);
+
+todos.removeAllTodo();
