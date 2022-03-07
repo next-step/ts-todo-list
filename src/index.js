330fd7ba26fea54ca3fc3fecbc04da51629c9e21
ppamppamman <ryou9305@naver.com>
2022-03-08 06:15:07 +0900

    fix: 주어진 docs 기준 구현

--- src/index.js
+++ src/index.js
@@ -22,8 +22,21 @@ const Todoitem = {};
  * @returns {{}}
  */
 const createItem = (id, content, completed, category, tags) => {
-	return {};
-}
+  const newItem = {
+    id,
+    content,
+    completed,
+    category,
+    tags: tags?.length ? tags : undefined,
+  };
+
+  Todoitems.push(newItem);
+  /** 본래 return 은 {}으로,
+   * 만들어진 Item객체를 리턴만 하는 것으로 이해했었으나,
+   * 전역으로 다뤄야하는 Todoitems에 넣는 method가 없어서 여기에 push를 구현함.
+   */
+  return newItem;
+};
 
 /**
  * 모든 할 일을 조회할 수 있다.
@@ -31,8 +44,8 @@ const createItem = (id, content, completed, category, tags) => {
  * @returns {Todoitem[]}
  */
 const readItems = () => {
-	return [];
-}
+  return Todoitems;
+};
 
 /**
  * ID를 기반으로 특정 할 일을 조회할 수 있다.
@@ -41,8 +54,9 @@ const readItems = () => {
  * @returns {{}}
  */
 const readItem = (id) => {
-	return {};
-}
+  const targetItem = Todoitems.find((item) => item.id === id);
+  return targetItem ? targetItem : {};
+};
 
 /**
  * ID를 제외한 모든 속성을 수정할 수 있다.
@@ -56,8 +70,18 @@ const readItem = (id) => {
  * @returns {boolean}
  */
 const updateItem = (id, content, completed, category, tags) => {
-	return true;
-}
+  const targetItemIdx = Todoitems.findIndex((item) => item.id === id);
+  if (targetItemIdx === -1) return false;
+
+  Todoitems[targetItemIdx] = {
+    ...Todoitems[targetItemIdx],
+    content,
+    completed,
+    category,
+    tags: tags.length ? tags : undefined,
+  };
+  return true;
+};
 
 /**
  * 특정 할 일의 특정 태그를 수정할 수 있다.
@@ -68,8 +92,17 @@ const updateItem = (id, content, completed, category, tags) => {
  * @returns {boolean}
  */
 const updateTag = (id, property, tobe) => {
-	return true;
-}
+  const targetItemIdx = Todoitems.findIndex((item) => item.id === id);
+  if (targetItemIdx === -1) return false;
+
+  const targetTagIdx = Todoitems[targetItemIdx].tags.findIndex(
+    (tag) => tag === property
+  );
+  if (targetTagIdx === -1) return false;
+  Todoitems[targetItemIdx].tags[targetTagIdx] = tobe;
+
+  return true;
+};
 
 /**
  * ID를 기반으로 특정 할 일을 삭제할 수 있다.
@@ -78,16 +111,21 @@ const updateTag = (id, property, tobe) => {
  * @returns {boolean}
  */
 const deleteItem = (id) => {
-	return true;
-}
+  const targetItemIdx = Todoitems.findIndex((item) => item.id === id);
+  if (targetItemIdx === -1) return false;
+
+  Todoitems.splice(targetItemIdx, 1);
+  return true;
+};
 
 /**
  * 모든 할 일을 제거할 수 있다.
  * @returns {boolean}
  */
 const deleteItems = () => {
-	return true;
-}
+  Todoitems = [];
+  return true;
+};
 
 /**
  * 특정 할 일의 특정 태그를 삭제할 수 있다.
@@ -96,8 +134,17 @@ const deleteItems = () => {
  * @returns {boolean}
  */
 const deleteTag = (id, tag) => {
-	return true;
-}
+  const targetItemIdx = Todoitems.findIndex((item) => item.id === id);
+  if (targetItemIdx === -1) return false;
+
+  const targetTagIdx = Todoitems[targetItemIdx].tags.findIndex(
+    (eachTag) => eachTag === tag
+  );
+  if (targetTagIdx === -1) return false;
+  Todoitems[targetItemIdx].tags.splice(targetTagIdx, 1);
+
+  return true;
+};
 
 /**
  * 특정 할 일의 모든 태그를 제거할 수 있다.
@@ -105,5 +152,9 @@ const deleteTag = (id, tag) => {
  * @returns {boolean}
  */
 const deleteTags = (id) => {
-	return true;
-}
+  const targetItemIdx = Todoitems.findIndex((item) => item.id === id);
+  if (targetItemIdx === -1) return false;
+
+  Todoitems[targetItemIdx].tags = [];
+  return true;
+};
