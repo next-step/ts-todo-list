053ad552082e65fcbbe3310b7b633c4c7ecedffa
kwonhoe1121 <kwonhoe1121@gmail.com>
2022-03-12 19:29:17 +0900

    index.d.ts 파일 추가

---
+++ @types/todo/index.d.ts
@@ -0,0 +1,21 @@
+export interface Todo {
+    id: string;
+    content: string;
+    isClear: boolean;
+    category: string;
+    tags?: string[];
+}
+
+export const todoList: Todo[];
+
+export function createTodo(properties: object): Todo;
+export function addTodo(todo: Todo): boolean;
+export function hasContent(todo: Todo): boolean;
+export function addTodoAfterValidation(todo: Todo, validationFn: (todo: Todo) => boolean): boolean;
+export function getTodoList(): Todo[];
+export function updateTodo(todoId: string, updateProperty: object): boolean;
+export function updateTag(todoId: string, targetTag: string, updateTag: string): boolean;
+export function deleteTodo(todoId: string): boolean;
+export function deleteTodoAll(): void;
+export function deleteTag(todoId: string, targetTag: string): boolean;
+export function deleteTags(todoId: string): boolean;
