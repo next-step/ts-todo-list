41be6fa774edc1d6d1ae0e54cb4b42b1b11a9039
g1moon <gmoon.cho@gmail.com>
2022-03-16 18:15:45 +0900

    refactor: index.d.ts 주석 처리

--- @types/todo/index.d.ts
+++ @types/todo/index.d.ts
@@ -6,16 +6,16 @@ export interface Todo {
     tags?: string[];
 }
 
-export const todoList: Todo[];
-
-export function createTodo(properties: object): Todo;
-export function addTodo(todo: Todo): boolean;
-export function hasContent(todo: Todo): boolean;
-export function addTodoAfterValidation(todo: Todo, validationFn: (todo: Todo) => boolean): boolean;
-export function getTodoList(): Todo[];
-export function updateTodo(todoId: string, updateProperty: object): boolean;
-export function updateTag(todoId: string, targetTag: string, updateTag: string): boolean;
-export function deleteTodo(todoId: string): boolean;
-export function deleteTodoAll(): void;
-export function deleteTag(todoId: string, targetTag: string): boolean;
-export function deleteTags(todoId: string): boolean;
+// export const todoList: Todo[];
+//
+// export function createTodo(properties: object): Todo;
+// export function addTodo(todo: Todo): boolean;
+// export function hasContent(todo: Todo): boolean;
+// export function addTodoAfterValidation(todo: Todo, validationFn: (todo: Todo) => boolean): boolean;
+// export function getTodoList(): Todo[];
+// export function updateTodo(todoId: string, updateProperty: object): boolean;
+// export function updateTag(todoId: string, targetTag: string, updateTag: string): boolean;
+// export function deleteTodo(todoId: string): boolean;
+// export function deleteTodoAll(): void;
+// export function deleteTag(todoId: string, targetTag: string): boolean;
+// export function deleteTags(todoId: string): boolean;
