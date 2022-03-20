03df07dc08be86f7d3a4f636c1fb6c2eaf706477
jinsoobyeon <jinsoobyeon@gmail.com>
2022-03-20 23:20:13 +0900

    Feat: 모든 기능 웹에서 조작 및 콘솔에서 확인

--- index.js
+++ index.js
@@ -11,32 +11,33 @@ class TodoList {
         console.table(this.list);
     }
     readItemById(id) {
-        console.table(this.list.find((item) => item.id === id));
+        console.table(this.list.find((item) => item.id === Number(id)));
     }
     updateItemById(id, option) {
         if (option.content) {
-            this.list.filter((item) => item.id === id)[0].content = option.content;
+            console.log(id);
+            console.log(option.content);
+            this.list.filter((item) => item.id === Number(id))[0].content =
+                option.content;
         }
         if (option.isFinished) {
-            this.list.filter((item) => item.id === id)[0].isFinished =
+            this.list.filter((item) => item.id === Number(id))[0].isFinished =
                 option.isFinished;
         }
         if (option.category) {
-            this.list.filter((item) => item.id === id)[0].category = option.category;
-        }
-        if (option.tags) {
-            this.list.filter((item) => item.id === id)[0].tags = option.tags;
+            this.list.filter((item) => item.id === Number(id))[0].category =
+                option.category;
         }
         this.readItemAll();
     }
     updateTagById(id, tagId, content) {
         this.list
-            .filter((item) => item.id === id)[0]
-            .tags.filter((tag) => tag.id === tagId)[0].content = content;
+            .filter((item) => item.id === Number(id))[0]
+            .tags.filter((tag) => tag.id === Number(tagId))[0].content = content;
         this.readItemAll();
     }
     deleteItemById(id) {
-        this.list = this.list.filter((item) => item.id !== id);
+        this.list = this.list.filter((item) => item.id !== Number(id));
         this.readItemAll();
     }
     deleteItemAll() {
@@ -44,13 +45,13 @@ class TodoList {
         this.readItemAll();
     }
     deleteTagById(id, tagId) {
-        this.list.filter((item) => item.id === id)[0].tags = this.list
-            .filter((item) => item.id === id)[0]
-            .tags.filter((tag) => tag.id !== tagId);
+        this.list.filter((item) => item.id === Number(id))[0].tags = this.list
+            .filter((item) => item.id === Number(id))[0]
+            .tags.filter((tag) => tag.id !== Number(tagId));
         this.readItemAll();
     }
     deleteTagAll(id) {
-        this.list.filter((item) => item.id === id)[0].tags = [];
+        this.list.filter((item) => item.id === Number(id))[0].tags = [];
         this.readItemAll();
     }
 }
@@ -61,9 +62,11 @@ class TodoItem {
         this.tags = [];
         this.content = content;
         this.category = category;
-        if (tags) {
-            this.tags = tags;
-        }
+        tags === null || tags === void 0 ? void 0 : tags.forEach((tag) => {
+            if (tag.content) {
+                this.tags.push(tag);
+            }
+        });
     }
 }
 class Tag {
@@ -72,17 +75,64 @@ class Tag {
         this.content = content;
     }
 }
-const javascript = new Tag("javascript");
-const typescript = new Tag("typesciprt");
-const react = new Tag("react");
-const todoItem = new TodoItem("할 일", "오늘 할 일");
-const todoItemWithTags = new TodoItem("할 일", "내일 할 일", [
-    javascript,
-    typescript,
-    react,
-]);
 const todoList = new TodoList();
+let isFinished = false;
 const addItem = () => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    const javascript = new Tag(document.getElementById("javascript").value);
+    const typescript = new Tag(document.getElementById("typescript").value);
+    const react = new Tag(document.getElementById("react").value);
+    const todoItem = new TodoItem(document.getElementById("content").value, document.getElementById("category").value, [javascript, typescript, react]);
     todoList.addItem(todoItem);
-    todoList.addItem(todoItemWithTags);
+};
+const readItemAll = () => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.readItemAll();
+};
+const readItemById = (id) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.readItemById(id);
+};
+const setTrue = () => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    isFinished = true;
+};
+const setFalse = () => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    isFinished = false;
+};
+const updateItemById = (id, option) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.updateItemById(id, option);
+};
+const updateTagById = (id, tagId, content) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.updateTagById(id, tagId, content);
+};
+const deleteItemById = (id) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.deleteItemById(id);
+};
+const deleteTagById = (id, tagId) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.deleteTagById(id, tagId);
+};
+const deleteTagAll = (id) => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.deleteTagAll(id);
+};
+const deleteItemAll = () => {
+    var _a;
+    (_a = window.event) === null || _a === void 0 ? void 0 : _a.preventDefault();
+    todoList.deleteItemAll();
 };
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ class TodoList {
   }
 
   readItemById(id: number): void {
-    console.table(this.list.find((item) => item.id === id));
+    console.table(this.list.find((item) => item.id === Number(id)));
   }
 
   updateItemById(
@@ -20,34 +20,34 @@ class TodoList {
       content?: string;
       isFinished?: boolean;
       category?: string;
-      tags?: Tag[];
     }
   ): void {
     if (option.content) {
-      this.list.filter((item) => item.id === id)[0].content = option.content;
+      console.log(id);
+      console.log(option.content);
+      this.list.filter((item) => item.id === Number(id))[0].content =
+        option.content;
     }
     if (option.isFinished) {
-      this.list.filter((item) => item.id === id)[0].isFinished =
+      this.list.filter((item) => item.id === Number(id))[0].isFinished =
         option.isFinished;
     }
     if (option.category) {
-      this.list.filter((item) => item.id === id)[0].category = option.category;
-    }
-    if (option.tags) {
-      this.list.filter((item) => item.id === id)[0].tags = option.tags;
+      this.list.filter((item) => item.id === Number(id))[0].category =
+        option.category;
     }
     this.readItemAll();
   }
 
   updateTagById(id: number, tagId: number, content: string): void {
     this.list
-      .filter((item) => item.id === id)[0]
-      .tags.filter((tag) => tag.id === tagId)[0].content = content;
+      .filter((item) => item.id === Number(id))[0]
+      .tags.filter((tag) => tag.id === Number(tagId))[0].content = content;
     this.readItemAll();
   }
 
   deleteItemById(id: number): void {
-    this.list = this.list.filter((item) => item.id !== id);
+    this.list = this.list.filter((item) => item.id !== Number(id));
     this.readItemAll();
   }
 
@@ -57,14 +57,14 @@ class TodoList {
   }
 
   deleteTagById(id: number, tagId: number): void {
-    this.list.filter((item) => item.id === id)[0].tags = this.list
-      .filter((item) => item.id === id)[0]
-      .tags.filter((tag) => tag.id !== tagId);
+    this.list.filter((item) => item.id === Number(id))[0].tags = this.list
+      .filter((item) => item.id === Number(id))[0]
+      .tags.filter((tag) => tag.id !== Number(tagId));
     this.readItemAll();
   }
 
   deleteTagAll(id: number): void {
-    this.list.filter((item) => item.id === id)[0].tags = [];
+    this.list.filter((item) => item.id === Number(id))[0].tags = [];
     this.readItemAll();
   }
 }
@@ -79,9 +79,11 @@ class TodoItem {
   constructor(content: string, category: string, tags?: Tag[]) {
     this.content = content;
     this.category = category;
-    if (tags) {
-      this.tags = tags;
-    }
+    tags?.forEach((tag) => {
+      if (tag.content) {
+        this.tags.push(tag);
+      }
+    });
   }
 }
 
@@ -94,20 +96,82 @@ class Tag {
   }
 }
 
-const javascript = new Tag("javascript");
-const typescript = new Tag("typesciprt");
-const react = new Tag("react");
-
-const todoItem = new TodoItem("할 일", "오늘 할 일");
-const todoItemWithTags = new TodoItem("할 일", "내일 할 일", [
-  javascript,
-  typescript,
-  react,
-]);
-
 const todoList = new TodoList();
 
+let isFinished = false;
+
 const addItem = () => {
+  window.event?.preventDefault();
+  const javascript = new Tag(
+    (document.getElementById("javascript") as HTMLInputElement).value
+  );
+  const typescript = new Tag(
+    (document.getElementById("typescript") as HTMLInputElement).value
+  );
+  const react = new Tag(
+    (document.getElementById("react") as HTMLInputElement).value
+  );
+  const todoItem = new TodoItem(
+    (document.getElementById("content") as HTMLInputElement).value,
+    (document.getElementById("category") as HTMLInputElement).value,
+    [javascript, typescript, react]
+  );
   todoList.addItem(todoItem);
-  todoList.addItem(todoItemWithTags);
+};
+
+const readItemAll = () => {
+  window.event?.preventDefault();
+  todoList.readItemAll();
+};
+
+const readItemById = (id: number) => {
+  window.event?.preventDefault();
+  todoList.readItemById(id);
+};
+
+const setTrue = () => {
+  window.event?.preventDefault();
+  isFinished = true;
+};
+
+const setFalse = () => {
+  window.event?.preventDefault();
+  isFinished = false;
+};
+
+const updateItemById = (
+  id: number,
+  option: {
+    content?: string;
+    isFinished?: boolean;
+    category?: string;
+  }
+) => {
+  window.event?.preventDefault();
+  todoList.updateItemById(id, option);
+};
+
+const updateTagById = (id: number, tagId: number, content: string) => {
+  window.event?.preventDefault();
+  todoList.updateTagById(id, tagId, content);
+};
+
+const deleteItemById = (id: number) => {
+  window.event?.preventDefault();
+  todoList.deleteItemById(id);
+};
+
+const deleteTagById = (id: number, tagId: number) => {
+  window.event?.preventDefault();
+  todoList.deleteTagById(id, tagId);
+};
+
+const deleteTagAll = (id: number) => {
+  window.event?.preventDefault();
+  todoList.deleteTagAll(id);
+};
+
+const deleteItemAll = () => {
+  window.event?.preventDefault();
+  todoList.deleteItemAll();
 };
