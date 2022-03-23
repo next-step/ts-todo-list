4e38fcdd9950018caf86ddba7e17a3fd4a4af43e
Autumn (조홍비) <60209518+hongbiii@users.noreply.github.com>
2022-03-24 00:21:53 +0900

    step4 구현 (#72)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { TodoList } from "todoModule";
+import { SingleTodo, TodoList } from "todoModule";
 
 /**
  * 하나의 SingleTodo 객체가 가지는 Property들 입니다.
@@ -14,15 +14,16 @@ import { TodoList } from "todoModule";
  * SingleTodo들을 담은 todoList 배열입니다.
  * @property {SingleTodoType[]} todoList
  */
-const todoList: TodoList = [];
+let todoList: TodoList = [];
 
 /**
  * @function addTodo
  * @param {SingleTodoType} newTodo
  * @description CRUD에서 C에 해당하는 함수입니다. todoList 배열에 새로운 newTodo 객체를 추가합니다. 리턴값은 없습니다.
  */
-function addTodo(newTodo) {
+function addTodo(newTodo: SingleTodo) {
   todoList.push(newTodo);
+
   console.log("=== [CREATE] 새로운 할 일이 추가되었습니다. ===");
   console.log(`👇 현재 Todo list 모두 보기 (총 ${todoList.length}개)`);
   console.log(JSON.stringify(todoList, null, 2));
@@ -35,7 +36,7 @@ function addTodo(newTodo) {
  * @description CRUD에서 R에 해당하는 함수입니다. param id가 있을 경우 id에 해당하는 특정 todo를 찾아 반환합니다. param id가 없을 경우 모든 할일 목록을 반환합니다.
  * @returns SingleTodoType[] | SingleTodo
  */
-function readTodo(id) {
+function readTodo(id?: number) {
   if (id) {
     const targetTodo = todoList.find((todo) => todo.id === id);
     console.log(`=== [READ] ID가 ${id}인 할 일을 읽습니다. ===`);
@@ -62,19 +63,17 @@ function readTodo(id) {
  * @param {string[]=} tags
  * @description CRUD에서 U에 해당하는 함수입니다. 수정하고자 하는 todo의 id와 수정하고자 하는 내용을 optional로 받아 전체 todoList에서 해당 todo의 특정 필드만 수정합니다. 리턴값은 없습니다.
  */
-function editTodo(id, content, category, isCompleted, tags) {
+
+type EditTodoParams = Pick<SingleTodo, 'id'> & Partial<Omit<SingleTodo, 'id'>>
+
+function editTodo(paramObj: EditTodoParams) {
+  const { id } = paramObj;
   const targetTodo = todoList.find((todo) => todo.id === id);
   const targetTodoIndex = todoList.findIndex((todo) => todo.id === id);
-  const paramObj = { id, content, category, isCompleted, tags };
-  const willUpdateFields = {};
 
-  for (const param in paramObj) {
-    if (paramObj[param]) {
-      willUpdateFields[param] = paramObj[param];
-    }
-  }
+  if (!targetTodo) return;
 
-  const todoAfterEdit = { ...targetTodo, ...willUpdateFields };
+  const todoAfterEdit: SingleTodo = { ...targetTodo, ...paramObj };
   todoList[targetTodoIndex] = todoAfterEdit; // todoList 배열 업데이트
 
   console.log(`=== [UPDATE] ID ${id}인 할 일의 내용을 수정합니다. ===`);
@@ -88,23 +87,9 @@ function editTodo(id, content, category, isCompleted, tags) {
  * @param {number} id
  * @description CRUD에서 D에 해당하는 함수입니다. 삭제하고자 하는 todo의 id를 받아, todoList에서 그 todo를 삭제합니다. 리턴값은 없습니다.
  */
-function deleteTodo(id) {
+function deleteTodo(id: number) {
   const targetTodoIndex = todoList.findIndex((todo) => todo.id === id);
-  const temp = [];
-
-  for (let i = todoList.length - 1; i >= 0; i--) {
-    if (i > targetTodoIndex) {
-      temp.push(todoList.pop());
-    }
-    if (i === targetTodoIndex) {
-      todoList.pop();
-      break;
-    }
-  }
-
-  while (temp.length) {
-    todoList.push(temp.pop());
-  }
+  todoList = todoList.filter(todo => todo.id !== id);
 
   console.log(`=== [DELETE] ID ${id}인 할 일을 삭제합니다. ===`);
   console.log(`👇 삭제 후 Todo list (총 ${todoList.length}개)`);
@@ -119,6 +104,7 @@ addTodo({
   isCompleted: false,
   tags: ["태그1", "태그2"],
 });
+
 addTodo({
   id: 2,
   content: "todo 2",
@@ -128,9 +114,13 @@ addTodo({
 });
 
 readTodo(1);
+
 readTodo(); // 파라미터 없이 호출하면 모든 투두리스트 읽기
 
-editTodo(1, "수정할 내용", "수정할 카테고리", true);
+editTodo({
+  id: 1,
+  category: "카테고리만 수정",
+});
 
 deleteTodo(1);
 
@@ -142,4 +132,4 @@ addTodo({
   tags: ["태그1", "태그2"],
 });
 
-editTodo(3, "", "", false, ["태그를 바꿀 때는 통째로 갈아끼워야 한다."]);
+editTodo({ id: 3, tags: ["태그를 바꿀 때는 통째로 갈아끼워야 한다."] });
